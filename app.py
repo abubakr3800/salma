b2b8a1e1b2cec4435ae5b5ee253d036e5b3b3f2e
abubakr3800 <ahmedabubakr148@gmail.com>
2025-08-09 00:31:32 +0300

    gunicorn

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import os
 
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # get port from env or fallback to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "uploads")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
