d9d05f716f5a8003d35b11f2e014c3d47349b6d1
abubakr3800 <ahmedabubakr148@gmail.com>
2025-08-09 01:12:41 +0300

    app

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ import os
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # get port from env or fallback to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
 
 UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "uploads")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
@@ -46,3 +43,7 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # get port from env or fallback to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
