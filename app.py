b4baf330ccac593160bdf514c91ad26fcbcb1d54
abubakr3800 <ahmedabubakr148@gmail.com>
2025-08-09 01:47:38 +0300

    app edited

--- app.py
+++ app.py
@@ -42,8 +42,5 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))  # get port from env or fallback to 5000
     app.run(host="0.0.0.0", port=port, debug=True)
